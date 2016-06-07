c0f58e5ea120f4683755a7afb2283da8700b2ff8
gictor <victor.lindellee@strossle.com>
2016-06-07 15:16:38 +0200

    Remove some logging

---
+++ test/unit-test.js
@@ -0,0 +1,35 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const strossleId = require('../src/index.js');
+
+describe('Generating', function () {
+
+    it('Should return one to many unique IDs', function (done) {
+
+        const uniqueArray = [];
+        const howManyTimesToRun = 1000;
+        for(let i = 0; i < howManyTimesToRun; i++) {
+            const generatedId = strossleId.generate();
+            expect(uniqueArray.indexOf(generatedId)).to.equal(-1);
+            uniqueArray.push(generatedId);
+        }
+        expect(uniqueArray.length).to.equal(howManyTimesToRun);
+        done();
+
+    });
+
+    it('Should consists of 38 characters', function (done) {
+        expect(strossleId.generate().length).to.equal(38);
+        done();
+    });
+
+});
+
+describe('Validating', function () {
+    it('Should fail too long string', function(done) {
+        const tooLongString = '203023948093jsfldjlsdkjflksdjfksdfjlk203402938420938lkfjslkdjflskdjf';
+        expect(strossleId.validateID(tooLongString)).to.equal(false);
+        done();
+    });
+});
