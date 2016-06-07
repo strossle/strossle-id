4c495dd59af80fba95bf95df1c8042896595459b
gictor <victor.lindellee@strossle.com>
2016-06-07 15:16:58 +0200

    Remove some logging

--- src/index.js
+++ src/index.js
@@ -7,32 +7,25 @@ const IDLength = 38;
 let rfc4122 = new RFC4122();
 
 const generate = function () {
-
     const versionPrefix = '0000';
     const versionAsHex = VERSION.toString(16);
-
     const versionAsfourBits = (versionPrefix+versionAsHex).slice(-4);
-    //console.log(versionAsfourBits);
     const hash = MetroHash128(VERSION).hash(rfc4122.v4()).toString('hex');
     const versionAsFourBitsAndHash = `${versionAsfourBits}${hash}`;
     const checksum = generateChecksum(versionAsFourBitsAndHash);
-
     return `${versionAsFourBitsAndHash}${checksum}`;
 };
 
 const validateID = function(id) {
-    console.log(id);
     if(id.length === IDLength) {
         const checksum = id.substr(-2);
         const idAndHash = id.substr(0, id.length-2);
         if (generateChecksum(idAndHash) === checksum) {
            return true;
         } else {
-            console.log("Wrong checksum, did Strossle generate this id, REALLY?");
             return false;
         };
     } else {
-        console.log("Wrong length");
         return false;
     }
 };
@@ -41,5 +34,7 @@ const generateChecksum = function (payload) {
     return crc.crc8(payload).toString(16);
 };
 
-const tempId = generate();
-validateID(tempId);
+module.exports = {
+    generate,
+    validateID,
+}
